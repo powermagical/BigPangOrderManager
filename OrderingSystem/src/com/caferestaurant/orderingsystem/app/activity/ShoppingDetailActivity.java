2c1e012baa05c9c8ebed3b8037fe01a81cc07c1e
送受漏 <powermagical2012@gmail.com>
2014-02-09 10:51:03 +0800

    添加一个菜单详细的baseActivity和layout（还没编译过交给激凸）
    送受漏

---
+++ OrderingSystem/src/com/caferestaurant/orderingsystem/app/activity/ShoppingDetailActivity.java
@@ -0,0 +1,26 @@
+package com.caferestaurant.orderingsystem.app.activity;
+
+import android.os.Bundle;
+
+/**
+ * 主界面activity
+ * 其中的各个view仅供参考
+ * @author Administrator
+ *
+ */
+public class ShoppingDetailActivity extends ShoppingDetailfBaseActivity{
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//super.setContentView(R.layout.main_layout);
+		this.initLayout();
+	}
+	
+	
+	private void initLayout()
+	{		
+	}
+	
+}
